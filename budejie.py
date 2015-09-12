ba0f3d304dc3ff9e2aeb441d86ba087d6223b661
wxsma <1269778817@qq.com>
2015-09-12 10:53:49 +0800

    fix encoding

--- budejie.py
+++ budejie.py
@@ -3,10 +3,17 @@
 import urllib2
 import re
 import sys
+reload (sys)
+sys.setdefaultencoding('utf8')  
+
 
 class budejie(object):
     """docstring for budejie"""
 
+    def __init__(self):
+
+        self.encoding = sys.getfilesystemencoding()
+        self.content = self.GetContent()
 
     def LoadPage(self):
         '''Return the html code. '''
@@ -15,10 +22,10 @@ class budejie(object):
         user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'
         header = {'User-Agent' : user_agent}
         request = urllib2.Request(url,headers = header)
+
         try:
             hpage = urllib2.urlopen(request)
-            encode = sys.getfilesystemencoding()
-            return hpage.read().encode(encode)
+            return hpage.read().decode('utf-8').encode(self.encoding)
         except urllib2.URLError as e:
             if hasattr(e,'code'):
                 print e.code
@@ -44,7 +51,7 @@ class budejie(object):
     def Print(self):
         '''Print the result from function GetContent. '''
 
-        items = self.GetContent()
+        items = self.content
         for item in items :
             print u'发布人: ',item[0]
             print item[1]
@@ -57,14 +64,16 @@ class budejie(object):
         '''Write the result from function GetContent into a file. 
            file:"E:\MyPython\\budejie.txt" is default. '''
 
-        items = self.GetContent()
+        items = self.content
         with open(filename,'w') as f:
             for item in items:
-                f.write(u'发布人: '+item[0]+'\n')
-                f.write(item[1]+'\n')
-                f.write(u'内容: '+item[2]+'\n')
-                f.write(u'点赞数: '+item[3]+'\n')
+                f.write(u'发布人: '+item[0].decode(self.encoding)+'\n')
+                f.write(item[1].decode(self.encoding)+'\n')
+                f.write(u'内容: '+item[2].decode(self.encoding)+'\n')
+                f.write(u'点赞数: '+item[3].decode(self.encoding)+'\n')
                 f.write('-'*40+'\n')
+
+        print 'ok'
 
 
 if __name__ == '__main__' :
